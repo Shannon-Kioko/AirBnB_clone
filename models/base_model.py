bb2cd938107ad9889c8ddb13124155a76d282e75
wxllxngton <jjngugi27@gmail.com>
2023-10-11 15:33:02 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -11,18 +11,40 @@ class BaseModel:
     This class defines common attributes and methods for other classes.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes a new BaseModel instance.
 
-        Attributes:
-            id (str): A unique identifier generated using uuid.uuid4().
-            created_at (datetime): The datetime when the instance is created.
-            updated_at (datetime): The datetime when the instance is created and updated.
+        Args:
+            *args: Unused arguments.
+            **kwargs: A dictionary containing attribute name-value pairs.
+
+        If kwargs is not empty, it initializes attributes from kwargs. 'created_at' and 'updated_at' values are
+        converted from string format to datetime objects. 'id' is set to a new uuid4 string.
+        If kwargs is empty, it creates 'id' and 'created_at' as in a new instance.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(
+                        self,
+                        key,
+                        datetime.datetime.strptime(
+                            value, "%Y-%m-%dT%H:%M:%S.%f"
+                        ),
+                    )
+                else:
+                    setattr(self, key, value)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            if "created_at" not in kwargs:
+                self.created_at = datetime.datetime.now()
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """
@@ -43,11 +65,9 @@ class BaseModel:
 
         Returns:
             dict: A dictionary containing all attributes of the instance.
-                  The '__class__' key is added with the class name.
                   'created_at' and 'updated_at' are converted to ISO format.
         """
         instance_dict = dict(self.__dict__)
-        instance_dict["__class__"] = self.__class__.__name__
         instance_dict["created_at"] = self.created_at.isoformat()
         instance_dict["updated_at"] = self.updated_at.isoformat()
         return instance_dict
