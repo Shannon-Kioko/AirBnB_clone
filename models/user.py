816637a7514e74f08231aeda933c87e311db154b
wxllxngton <jjngugi27@gmail.com>
2023-10-12 11:51:28 +0300

    Add New File user.py

---
+++ models/user.py
@@ -0,0 +1,15 @@
+"""
+This module contains the class User
+"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """
+    This class defines the User model with email, password, first_name, and last_name attributes.
+    """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
