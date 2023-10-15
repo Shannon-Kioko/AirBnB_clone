fa26ccc33d584fa1cf2d8138b6cd5cf0c5006e61
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-10-15 11:27:37 +0300

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This modeule defines the FileStorage class.
+This module defines the FileStorage class.
 """
 import json
 from models.base_model import BaseModel
@@ -10,7 +10,6 @@ from models.city import City
 from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
-
 
 class FileStorage:
     """Represent an abstracted storage engine.
@@ -37,24 +36,24 @@ class FileStorage:
         Args:
             obj (BaseModel): The object to be stored.
         """
-        ocname = obj.__class__.__name__
-        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
+        object_class_name = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(object_class_name, obj.id)] = obj
 
     def save(self):
         """Serialize __objects to the JSON file __file_path."""
-        odict = FileStorage.__objects
-        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
-        with open(FileStorage.__file_path, "w") as f:
-            json.dump(objdict, f)
+        current_objects = FileStorage.__objects
+        serialized_objects = {obj: current_objects[obj].to_dict() for obj in current_objects.keys()}
+        with open(FileStorage.__file_path, "w") as file:
+            json.dump(serialized_objects, file)
 
     def reload(self):
         """Deserialize the JSON file __file_path to __objects, if it exists."""
         try:
-            with open(FileStorage.__file_path) as f:
-                objdict = json.load(f)
-                for o in objdict.values():
-                    cls_name = o["__class__"]
-                    del o["__class__"]
-                    self.new(eval(cls_name)(**o))
+            with open(FileStorage.__file_path) as file:
+                deserialized_objects = json.load(file)
+                for object_data in deserialized_objects.values():
+                    class_name = object_data["__class__"]
+                    del object_data["__class__"]
+                    self.new(eval(class_name)(**object_data))
         except FileNotFoundError:
             return
