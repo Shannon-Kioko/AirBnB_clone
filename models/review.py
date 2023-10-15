4dfb73ccda86f11f3eb13eb3322f78beaa25e388
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-10-15 10:45:55 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -8,7 +8,8 @@ from models.base_model import BaseModel
 
 class Review(BaseModel):
     """
-    This class defines the Review model. Attributes:
+    This class defines the Review model. 
+    Attributes:
         place_id (str): The Place id.
         user_id (str): The User id.
         text (str): The text of the review.
